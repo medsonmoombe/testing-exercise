7fc4f1a2be096e4024a7544fc57d8bfe5e79c502
medsonmoombe <medsonmoombe21@gmail.com>
2022-05-13 11:50:51 +0200

    Addand modify the linters

--- reverseString.test.js
+++ reverseString.test.js
@@ -1,4 +1,4 @@
-const reverseString= require('./src/reverseString.js');
+const reverseString = require('./src/reverseString.js');
 
 test('to check if string is reverssed', () => {
   const str = 'gender';
